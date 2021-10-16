d2e2955f06d0dceb768df9a86805f11cfd58d361
Berker <berkergng457@gmail.com>
2021-10-16 20:55:55 +0300

    Folders added

--- 5_kyu/ValidParentheses.java
+++ 5_kyu/ValidParentheses.java
@@ -1,10 +1,9 @@
-public class Solution{
-  
-  public static boolean validParentheses(String parens)
-  {
+public class ValidParentheses {
+
+  public static boolean validParentheses(String parens) {
     // opening parentheses count
     int openingCount = 0;
-    
+
     // iterate over String for looking parentheses
     for (int i = 0; i < parens.length(); i++) {
       // for opening parentheses, increment openingCount
@@ -13,12 +12,14 @@ public class Solution{
       // for closing parentheses, decrement openingCount
       else if (parens.charAt(i) == ')') {
         // if string has closing parenthes without opening parenthes, return false
-        if (openingCount == 0)  return false;
+        if (openingCount == 0)
+          return false;
         openingCount--;
       }
     }
     // if size of opening parentheses and closing parentheses are equal, return true
-    if (openingCount == 0)  return true;
+    if (openingCount == 0)
+      return true;
     return false;
   }
 }
